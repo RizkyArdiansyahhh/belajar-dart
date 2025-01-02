ffa4cf986588efcefaaae083334e1a552283f070
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-02 16:26:09 +0700

    switch case

---
+++ bin/switch_case.dart
@@ -0,0 +1,24 @@
+main() {
+  var nilai = "A";
+
+  switch (nilai) {
+    case "A":
+      print("Lulus");
+      break;
+    case "B":
+      print("Lulus");
+      break;
+    case "C":
+      print("Lulus");
+      break;
+    case "D":
+      print("Tidak Lulus");
+      break;
+    case "E":
+      print("Tidak Lulus");
+      break;
+    default:
+      print("Tidak Lulus");
+      break;
+  }
+}
