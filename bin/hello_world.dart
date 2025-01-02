258cd3536df5aa8d2b8ab1cee711fff6d696a74c
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-02 15:03:51 +0700

    list

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,5 +1,3 @@
 main() {
-  String input = "1000";
-  int inputInt = int.parse(input);
-  double inputDouble = double.parse(input);
+  String name = "dart";
 }
---
+++ bin/list.dart
@@ -0,0 +1,17 @@
+main() {
+  List<String> names = ["Rizky", "budi", "budo"];
+
+  names.add("andre");
+  names.addAll(["andi", "budi"]);
+
+// For biasa
+  // for (int i = 0; i < names.length; i++) {
+  //   names.remove("budi");
+  // }
+  // print(names);
+
+  // For each
+  names.forEach((element) {
+    print(element);
+  });
+}
