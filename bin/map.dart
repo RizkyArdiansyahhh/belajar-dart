e5f26151312cb345dc6d1765178f1e8d5ec63bd2
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-05-03 20:41:05 +0700

    learn : Map

--- bin/map.dart
+++ bin/map.dart
@@ -1,10 +1,23 @@
 main() {
-  var name = <String, String>{};
+  Map<String, String> biodata = {
+    "nama": "Rizky ardiansyah",
+    "umur": "12",
+    "alamat": "pekanbaru",
+  };
 
-  name["first"] = "rizky";
-  name["midle"] = "ardiansyah";
-  name["last"] = "pasaribu";
+  List<MapEntry<String, String>> entries = [
+    MapEntry("semester", "6"),
+    MapEntry("alamat", "12345678910"),
+  ];
 
-  name.remove("last");
-  print(name);
+  biodata.addEntries(entries);
+  // print(biodata);
+
+  // for (int i = 0; i < biodata.length; i++) {
+  //   print(biodata.entries.toList()[i]);
+  // }
+
+  for (MapEntry<String, String> bio in biodata.entries) {
+    print(bio);
+  }
 }
